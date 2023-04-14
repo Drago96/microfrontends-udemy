d6c4153834739b396a11a72a1683249bd5e2cc8f
Drago96 <dragproychev@gmail.com>
2023-04-14 19:27:46 +0300

    Add initial mfp application

---
+++ mfp/packages/marketing/src/bootstrap.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+export const mount = (element) => {
+  ReactDOM.render(<App />, element);
+};
+
+var mountElement = document.querySelector("#_marketing-dev-root");
+
+if (process.env.NODE_ENV === "development" && mountElement) {
+  mount(mountElement);
+}
