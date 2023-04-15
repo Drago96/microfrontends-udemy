393408c3573476dffdef7e3003be800a0c6397da
Drago96 <dragproychev@gmail.com>
2023-04-15 18:49:18 +0300

    Add header to container

--- mfe/packages/container/src/App.js
+++ mfe/packages/container/src/App.js
@@ -1,7 +1,23 @@
+import { StylesProvider, createGenerateClassName } from "@material-ui/styles";
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
+import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 export default function App() {
-  return <MarketingApp />;
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  );
 }
